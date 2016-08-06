92ca35622d899c37257618dbebf275ec7b29b2dc
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-06 15:05:18 +0300

    Initial structure and core types

---
+++ src/Eff.Core/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Eff.Core.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Eff.Core")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Eff.Core")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("debe38c8-506d-4cf0-934c-9210cdeffe8c")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ src/Eff.Core/CoreTypes.fs
@@ -0,0 +1,33 @@
+﻿namespace Eff.Core
+
+open System
+
+// Core types and Builder
+
+type Eff<'T, 'R> = Eff of ((('T -> 'R) * (exn -> 'R)) -> 'R)
+
+// Annotation type for Effects
+type Effect() =
+    inherit Exception()
+
+// Basic builder 
+type EffBuilder() = 
+    member self.Return (x : 'T) : Eff<'T, 'R> = Eff (fun (k, _) -> k x)
+    member self.ReturnFrom (eff : Eff<'T, 'R>) : Eff<'T, 'R> = eff
+    member self.Bind (eff : Eff<'A, 'R>, f : 'A -> Eff<'B, 'R>) : Eff<'B, 'R> =
+        Eff (fun (k, exk) -> let (Eff cont) = eff in cont ((fun v -> let (Eff cont') = f v in cont' (k, exk)), exk))
+    member self.TryWith (eff : Eff<'T, 'R>, f : exn -> Eff<'T, 'R>) : Eff<'T, 'R> =
+        Eff (fun (k, exk) -> 
+                let (Eff cont) = eff
+                cont (k, (fun ex -> 
+                    match (try Choice1Of2 (f ex) with ex -> Choice2Of2 ex) with
+                    | Choice1Of2 (Eff cont') -> cont' (k, exk)
+                    | Choice2Of2 ex -> exk ex)))
+     member self.Delay (f : unit -> Eff<'T, 'R>) : Eff<'T, 'R> = 
+        Eff (fun (k, exk) -> let (Eff cont) = f () in cont (k, exk))
+
+
+[<AutoOpen>]
+module Eff = 
+
+    let eff = new EffBuilder()
