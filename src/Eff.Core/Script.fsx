14c15ef821aef956af65c6134387030d377e696b
palladin <nick.palladinos@gmail.com>
2016-08-10 18:13:44 +0300

    Update handlers to propagate the unhandled effects

--- src/Eff.Core/Script.fsx
+++ src/Eff.Core/Script.fsx
@@ -18,16 +18,17 @@ let test () : Eff<int, Effect> =
     } 
 
 let stateHandler (s : 'S) (eff : Eff<'T, Effect>) : Eff<'T * 'S, Effect> =
-    let rec loop (s : 'S) (effect : Effect) = 
+    let rec loop (s : 'S) (k : 'T * 'S -> Effect) (effK : Effect -> Effect) (effect : Effect) : Effect = 
         match effect with
-        | :? Get<'S, Effect> as get -> loop s (get.K s) 
-        | :? Put<'S, Effect> as put -> loop put.Value (put.K ())
-        | :? Done<'T> as done' -> (done'.Value, s)
-        | _ -> failwith "Unhandled effect"
+        | :? Get<'S, Effect> as get -> loop s k effK (get.K s) 
+        | :? Put<'S, Effect> as put -> loop put.Value k effK (put.K ())
+        | :? Done<'T> as done' -> k (done'.Value, s)
+        | _ -> effK effect
     Eff (fun (k, exK, effK) -> 
                 let (Eff cont) = eff 
-                let effect = cont (done', exK, effK)
-                k <| loop s effect) 
+                let effK' = loop s k effK
+                let effect = cont (done', exK, effK')
+                effK' effect) 
 
 test () |> stateHandler 1 |> run // (4, 4)
 
@@ -41,19 +42,21 @@ let nonDetTest () : Eff<int * string, Effect> =
     }
 
 let nonDetHandler (eff : Eff<'T, Effect>) : Eff<seq<'T>, Effect> =
-    let rec loop (effect : Effect) : seq<'T> = 
+    let rec loop (k : seq<'T> -> Effect) (effK : Effect -> Effect) (effect : Effect) : Effect = 
         match effect with
         | :? NonDetEffect<Effect> as nonDet -> 
-            let results = nonDet.Invoke <| { new NonDetUnPack<Effect, seq<'T>> with
-                                                member self.Invoke k = loop <| k ()
-                                                member self.Invoke<'C>(first : 'C, second : 'C, k : 'C -> Effect) =
-                                                    seq { yield! loop (k first); yield! loop (k second) }  }
+            let results = nonDet.Invoke <| { new NonDetUnPack<Effect, Effect> with
+                                                member self.Invoke k' = 
+                                                    loop k effK (k' ())
+                                                member self.Invoke<'C>(first : 'C, second : 'C, k' : 'C -> Effect) =
+                                                    loop (fun seqs -> loop (fun seqs' -> k <| Seq.append seqs seqs') effK (k' second)) effK (k' first) }
             results
-        | :? Done<'T> as done' -> Seq.singleton done'.Value
-        | _ -> failwith "Unhandled effect"
+        | :? Done<'T> as done' -> k <| Seq.singleton done'.Value
+        | _ -> effK effect
     Eff (fun (k, exK, effK) -> 
             let (Eff cont) = eff 
-            let effect = cont (done', exK, effK)
-            k <| loop effect)
+            let effK' = loop k effK
+            let effect = cont (done', exK, effK')
+            loop k effK' effect)
     
 nonDetTest () |> nonDetHandler |> run // seq [(1, "1"); (1, "2"); (2, "1"); (2, "2")]
