2dfa8217c3cff9c2d60f2426286453deeca4bdf1
Nick Palladinos <nick.palladinos@gmail.com>
2016-08-21 18:41:21 +0300

    Add Hilbert's epsilon

--- src/Eff.Core/Script.fsx
+++ src/Eff.Core/Script.fsx
@@ -4,11 +4,13 @@
 #load "NonDet.fs"
 #load "Log.fs"
 #load "Concurrency.fs"
+#load "Searcher.fs"
 open Eff.Core
 open Eff.Core.State
 open Eff.Core.NonDet
 open Eff.Core.Log
 open Eff.Core.Concurrent
+open Eff.Core.Searcher
 
 // State examples
 let stateTest () : Eff<int, Effect> = 
@@ -88,3 +90,21 @@ let rec concurrentTest id depth =
 
 concurrentTest 0 2 |> pureLogHandler<unit, string> |> sequentialHandler |> run 
 concurrentTest 0 2 |> consoleLogHandler<unit, string> |> threadPoolHandler |> run 
+
+// http://math.andrej.com/2011/12/06/how-to-make-the-impossible-functionals-run-even-faster/
+let findNeighborhood (p : (int -> Eff<bool, Effect>) -> Eff<bool, Effect>) : Eff<bool, Effect> =
+    p (fun n -> search n)
+
+let epsilon p n = 
+    eff {
+        let! (_, s) = findNeighborhood p |> findNeighborhoodHandler
+        let result = s |> List.tryFind (fun (n', b) -> n = n')
+        match result with 
+        | Some (_, b) -> return b
+        | None -> return true
+    }
+
+let exists p = p (epsilon p)
+
+let f = epsilon (fun f -> eff { let! x = f 10 in let! y = f 11 in return x <> y }) 
+[ for i in [0..10] -> run <| f i ] // [true; true; true; true; true; true; true; true; true; true; false]
---
+++ src/Eff.Core/Searcher.fs
@@ -0,0 +1,36 @@
+﻿namespace Eff.Core
+
+// basic operation
+type Search<'S, 'Eff when 'Eff :> Effect>(v : 'S, k : bool -> 'Eff) =
+    interface Effect
+    member self.Value = v
+    member self.K = k
+
+
+module Searcher = 
+
+    // helper functions
+    let search (v : 'S) : Eff<bool, Effect> =
+        shift (fun k -> new Search<'S, Effect>(v, k) :> _)
+
+    // effect handlers
+    let findNeighborhoodHandler (eff : Eff<bool, Effect>) : Eff<bool * list<int * bool>, Effect> =
+        let rec loop (s : list<int * bool>) (resultK : bool * list<int * bool> -> Effect) (effK : Effect -> (Effect -> EffCont<Effect> -> Effect) -> Effect) (effect : Effect) (k : Effect -> EffCont<Effect> -> Effect) : Effect = 
+            match effect with
+            | :? Search<int, Effect> as search -> 
+                let result = s |> List.tryFind (fun (n, b) -> n = search.Value)
+                match result with
+                | Some (n, b) -> loop s resultK effK (search.K b) k
+                | None -> loop ((search.Value, true) :: s) (function (true, s') -> resultK (true, s') 
+                                                                   | (false, _) -> loop ((search.Value, false) :: s) resultK effK (search.K false) k) 
+                               effK (search.K true) k
+            | :? Done<bool> as done' -> 
+                let effect' = resultK (done'.Value, s)
+                effK effect' (fun effect' (EffCont effK') -> k effect' (EffCont (loop s resultK effK')))
+            | _ ->
+                effK effect (fun effect' (EffCont effK') -> k effect' (EffCont (loop s resultK effK')))
+        Eff (fun (k, exK, EffCont effK) -> 
+                    let (Eff cont) = eff 
+                    let effK' = loop [] k effK
+                    let effect = cont (done', exK, EffCont effK')
+                    runEffCont effect (EffCont effK')) 
