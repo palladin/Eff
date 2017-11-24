6a8ec36831dded3dd2e883d4e067cb915eb77fbe
palladin <nick.palladinos@gmail.com>
2017-11-24 19:30:34 +0200

    Cleanup

--- src/Eff.Core/NonDet.fs
+++ src/Eff.Core/NonDet.fs
@@ -7,7 +7,7 @@ and NonDetUnPack<'R> =
     abstract Invoke : (unit -> Effect) -> 'R 
     abstract Invoke<'T> : 'T * 'T * ('T -> Effect) -> 'R
 
-//// Basic non-determinism operations
+// Basic non-determinism operations
 and Choose<'T>(first : 'T, second : 'T, k : 'T -> Effect) =
     interface NonDetEffect with
         override self.Invoke unpack = 
@@ -29,8 +29,8 @@ module NonDet =
 
     let fail<'U when 'U :> NonDetEffect> () : Eff<'U, unit> = 
         shift (fun k -> new Fail(k) :> _)
-//
-//    // non-determinism effect handlers
+
+    // non-determinism effect handlers
     let nonDetHandler<'U, 'A when 'U :> NonDetEffect> 
         : Eff<'U, 'A> -> Eff<'U, list<'A>> = 
         fun eff ->
@@ -53,23 +53,4 @@ module NonDet =
             let (Eff effK) = eff
             let effect = effK done'
             Eff (fun k -> loop k effect)
-//    let nonDetHandler (eff : Eff<'T, Effect>) : Eff<list<'T>, Effect> =
-//        let rec loop (resultK : list<'T> -> Effect) (effK : Effect -> (Effect -> EffCont<Effect> -> Effect) -> Effect) (effect : Effect) (k : Effect -> EffCont<Effect> -> Effect) : Effect = 
-//            match effect with
-//            | :? NonDetEffect<Effect> as nonDet -> 
-//                let results = nonDet.Invoke <| { new NonDetUnPack<Effect, Effect> with
-//                                                    member self.Invoke k' = 
-//                                                        resultK []
-//                                                    member self.Invoke<'C>(first : 'C, second : 'C, k' : 'C -> Effect) =
-//                                                        loop (fun seqs -> loop (fun seqs' -> resultK <| List.append seqs seqs') effK (k' second) k) effK (k' first) k }
-//                results
-//            | :? Done<'T> as done' -> 
-//                let effect' = resultK [done'.Value]
-//                effK effect' (fun effect' (EffCont effK') -> k effect' (EffCont (loop resultK effK')))
-//            | _ -> 
-//                effK effect (fun effect' (EffCont effK') -> k effect' (EffCont (loop resultK effK')))
-//        Eff (fun (k, exK, EffCont effK) -> 
-//                let (Eff cont) = eff 
-//                let effK' = loop k effK
-//                let effect = cont (done', exK, EffCont effK')
-//                runEffCont effect (EffCont effK'))
+
